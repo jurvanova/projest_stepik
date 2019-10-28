0bd160cd821dfc67836450b1dc3e06533b0229ff
jurvanova <jurvanova@gmail.com>
2019-10-28 22:07:58 +0300

    Change for function is_element_present

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,7 +2,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
     
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
@@ -10,9 +10,9 @@ class BasePage():
     def open(self):
         self.browser.get(self.url)
         
-    def is_element_present(self, css):
+    def is_element_present(self, how, what):
         try:
-            self.browser.find_element_by_css_selector(css)
+            self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
         return True
