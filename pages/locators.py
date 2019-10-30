74d2139d3aab2e63cdde31406ec30738554ce4b6
jurvanova <jurvanova@gmail.com>
2019-10-30 17:24:14 +0300

    Add locators for product page testing

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class LoginPageLocators():
     FORM_SELECTOR = (By.CSS_SELECTOR, '#login_form')
     REGISTER_SELECTOR = (By.CSS_SELECTOR, '#register_form')
     
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    PRODUCT_NAME_SELECT = (By.CSS_SELECTOR, '.product_main h1')
+    PRODUCT_PRICE_SELECT = (By.CSS_SELECTOR, '.product_main .price_color')
+    
+    PRODUCT_NAME_MESSAGE = (By.CSS_SELECTOR, '#messages div:nth-child(1) strong')
+    PRODUCT_PRICE_MESSAGE = (By.CSS_SELECTOR, '#messages p:nth-child(1) strong')
