17df7541053fa237d6c5d08d76c32e45d038ac2b
jurvanova <jurvanova@gmail.com>
2019-10-30 19:56:16 +0300

    Fix cheking price

--- pages/product_page.py
+++ pages/product_page.py
@@ -5,20 +5,23 @@ class ProductPage(BasePage):
     
     def should_add_product_to_cart(self):
         self.should_check_cart_button()
-        PRODUCT_NAME = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_SELECT).text
-        PRODUCT_PRICE = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_SELECT).text
-        print(PRODUCT_NAME, PRODUCT_PRICE)
         self.element_click(*ProductPageLocators.ADD_TO_BASKET)
 
     def should_check_product_in_cart(self):
+        PRODUCT_NAME = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_SELECT).text
+        print(PRODUCT_NAME)
         self.should_check_product_name(PRODUCT_NAME)
+        
+    def should_check_price_in_cart(self):
+        PRODUCT_PRICE = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_SELECT).text
+        print(PRODUCT_PRICE)
         self.should_check_product_price(PRODUCT_PRICE)
         
     def should_check_cart_button(self):
-        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET), 'Button is not found'
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET), 'Button add to cart is not found'
     
     def should_check_product_name(self, param):
-        assert param == self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_MESSAGE).text, 'Product is incorrect'
+        assert param == self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_MESSAGE).text, 'Product name is incorrect'
         
     def should_check_product_price(self, param):
         assert param == self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_MESSAGE).text, 'Price of product is incorrect'
--- test_product_page.py
+++ test_product_page.py
@@ -3,8 +3,8 @@
 from .pages.product_page import ProductPage
 import time
 
-LINK = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-#LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+#LINK = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
 
 def test_should_add_product_to_cart(browser):
     page = ProductPage(browser, LINK)
@@ -14,4 +14,5 @@ def test_should_add_product_to_cart(browser):
     page.solve_quiz_and_get_code()
     time.sleep(5)
     page.should_check_product_in_cart()
+    page.should_check_price_in_cart()
     
