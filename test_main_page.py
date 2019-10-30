d210b90b5c6d66e56bfe6c6218ed46239d7c34a1
jurvanova <jurvanova@gmail.com>
2019-10-30 17:24:51 +0300

    Add minor changes

--- test_main_page.py
+++ test_main_page.py
@@ -5,6 +5,7 @@ from .pages.login_page import LoginPage
 
 LINK = 'http://selenium1py.pythonanywhere.com/'
 #LINK = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer '
+
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, LINK)
     page.open()
@@ -16,4 +17,5 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, LINK)
     page.open()
     page.should_be_login_link()
+    
 
